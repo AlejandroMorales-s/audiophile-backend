ff9d2feed72c7d513c78e0195aea64e42f49f292
Alejandro <alejandrom.am33@gmail.com>
2023-04-13 18:17:51 -0600

    cors configured

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(
   session({
-    secret: "secret",
-    cookie: { maxAge: 60000, secure: false, sameSite: "none" },
+    secret: "tnsclxgdl.aopsnaoethu",
+    cookie: { maxAge: 172800000, secure: true, sameSite: "none" },
     saveUninitialized: false,
     resave: false,
   })
