550299ee3814a6ccbaeeb647b0f6432892c9c300
Alejandro <alejandrom.am33@gmail.com>
2023-04-15 02:39:33 -0600

    allowing credentials - defined origin urls in cors

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 
 const sessionConfig = {
   secret: "tnsclxgdl.aopsnaoethu",
