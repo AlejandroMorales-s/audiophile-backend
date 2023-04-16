beb13eef49607747bf1f746f471cac6d81888354
Alejandro <alejandrom.am33@gmail.com>
2023-04-15 20:51:29 -0600

    added methods and content type in cors

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(
   cors({
     origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     headers: { "Access-Control-Allow-Origin": "*" },
   })
 );
