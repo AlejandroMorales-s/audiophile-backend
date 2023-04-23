3927b36f60b9c187c92f2e83a2409e96d2839a1e
Alejandro <alejandrom.am33@gmail.com>
2023-04-23 02:33:13 -0600

    renamed sentiment analysis router file

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const apiRouter = require("express").Router();
 
 apiRouter.use("/auth", require("./authRouter"));
 apiRouter.use("/products", require("./productsRouter"));
-apiRouter.use("/sentiment-analysis", require("./sentimentAnalysis"));
+apiRouter.use("/sentiment-analysis", require("./sentimentAnalysisRouter"));
 apiRouter.use("/shopping-cart", require("./shoppingCartRouter"));
 
 module.exports = apiRouter;
