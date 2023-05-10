ab1dc0e7f3a3396748ad5be1379a401f401ce2bb
Alejandro <alejandrom.am33@gmail.com>
2023-05-10 17:32:23 -0600

    added stripe router for payments

---
+++ src/stripeRouter.js
@@ -0,0 +1,22 @@
+const calculateOrderAmount = require("./helpers/calculateOrderAmount");
+
+const stripeRouter = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
+
+stripeRouter.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items).grandTotal * 100,
+    currency: "usd",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+module.exports = stripeRouter;
