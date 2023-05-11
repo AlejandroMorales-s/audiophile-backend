465a71cdf110fd2cb852b26b3ea7b8c4f66533e0
Alejandro <alejandrom.am33@gmail.com>
2023-05-11 02:22:14 -0600

    removed same site

--- src/stripeRouter.js
+++ src/stripeRouter.js
@@ -4,21 +4,23 @@ const stripeRouter = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 stripeRouter.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+  try {
+    const { items } = req.body;
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items).grandTotal * 100,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items).grandTotal * 100,
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
 module.exports = stripeRouter;
