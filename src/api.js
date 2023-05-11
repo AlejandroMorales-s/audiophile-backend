9df00e7e2e1b6887b7d5d850bdd871036052ec6a
Alejandro <alejandrom.am33@gmail.com>
2023-05-11 00:36:47 -0600

    removed middleware in payment route

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ const { ensureAuthenticated } = require("./middlewares");
 apiRouter.use("/auth", require("./authRouter"));
 apiRouter.use("/products", require("./productsRouter"));
 apiRouter.use("/sentiment-analysis", require("./sentimentAnalysisRouter"));
+apiRouter.use("/payment", require("./stripeRouter"));
 
 //* Apply ensureAuthenticated middleware to the routers that require authentication
 apiRouter.use(
@@ -15,6 +16,5 @@ apiRouter.use(
 apiRouter.use("/wishlist", ensureAuthenticated, require("./wishlistRouter"));
 apiRouter.use("/user", ensureAuthenticated, require("./userRouter"));
 apiRouter.use("/orders", ensureAuthenticated, require("./ordersRouter"));
-apiRouter.use("/payment", ensureAuthenticated, require("./stripeRouter"));
 
 module.exports = apiRouter;
