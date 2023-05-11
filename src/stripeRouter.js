bb81ca0e68d907d635401baccc168347be9678af
Alejandro <alejandrom.am33@gmail.com>
2023-05-11 00:31:31 -0600

    added headers in stripe router

--- src/stripeRouter.js
+++ src/stripeRouter.js
@@ -6,6 +6,13 @@ const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 stripeRouter.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
 
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://audiophile-ecommerce-chi.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items).grandTotal * 100,
     currency: "usd",
